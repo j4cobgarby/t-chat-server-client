9b24fd73f99702776e1792c38fa59e875ca6c600
Jacob Garby <j.garby@icloud.com>
2017-12-16 19:17:56 +0000

    more cross-platform ansi code

--- constants.hpp
+++ constants.hpp
@@ -7,8 +7,8 @@
 #define NET_MSG_SIZE 1024 // bytes
 #define NET_PORT_DEFAULT 53000
 
-std::ostream& bold_on(std::ostream& os) { return os << "\e[1m"; }
-std::ostream& bold_off(std::ostream& os) { return os << "\e[0m"; }
+std::ostream& bold_on(std::ostream& os) { return os << "\033[1m"; }
+std::ostream& bold_off(std::ostream& os) { return os << "\033[0m"; }
 
 inline bool file_exists(const std::string& name) {
     std::ifstream f(name.c_str());
